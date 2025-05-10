dca3b5c04867a778dbe3591a62c0ab53b13bc144
Caarl001 <leonardovenegas763@gmail.com>
2025-05-10 11:52:31 -0400

    Add scripts.js y sus metodos

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// console.log('Funciona correctamente');
+
+// // QuerrySelector
+// const header = document.querySelector('#heading'); // retorna 0 o un elemento
+// console.log(header);
+
+// // querySelectorAll
+// const enlaces = document.querySelectorAll('.navegacion a');
+// console.log(enlaces[0]);
+// // enlaces[0].textContent = "wea";
+
+// //getELementById
+//  const heading2 = document.getElementById('heading');
+//  console.log(heading2);
+
+
+
+
+// // Generar codigo HTML
+//   const nuevoEnlace = document.createElement('A');
+
+// // Agregando el href
+//     nuevoEnlace.href ='nuevo-enlace.html';
+
+// // Agregar la clase
+//     nuevoEnlace.classList.add('navegacion__enlace');
+
+// // Agregar el texto
+//     nuevoEnlace.textContent = "Nuevo Enlace";
+
+// // Agregarlo al documento
+// const navegacion = document.querySelector('.navegacion');
+// navegacion.appendChild(nuevoEnlace);
+    
+//   console.log(nuevoEnlace);
+
+//   // 
+
+//   console.log(1);
+//   window.addEventListener('load', function() {
+      
+//       console.log(2);
+//   });
+
+//   window.onload = function() {
+//     console.log(3);
+
+//   }
+
+//    document.addEventListener('DOMContentLoaded' , function() {
+//     console.log(4);
+//    });
+//   console.log(5);
+
+// window.onscroll = function() {
+//     console.log('scrolling....');
+// }
